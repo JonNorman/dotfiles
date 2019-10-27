f53699b306433149a335468cb03c78b2ab578650
Jon Norman <jon.norman@theguardian.com>
2019-10-27 16:27:45 +0000

    Remove old config values for previous Mac OS versions.

--- .bashrc
+++ .bashrc
@@ -1,8 +1,4 @@
 alias ls='ls -G'
-alias dirs='dirs -v'
-alias ll='ls -l'
-alias la='ls -a'
-alias lal='ls -la'
 alias rm='rm -i'
 
 # colourify ls
@@ -11,24 +7,10 @@ export CLICOLOR=1
 export LSCOLORS=CaeaxxxxBxxxxxxxxxCaCa
 
 export PAGER=less
-export HISTFILESIZE=5000          # Store 5000 commands in history
+export HISTFILESIZE=10000          # Store 5000 commands in history
 export HISTCONTROL=ignoredups:ignorespace    # Don't put duplicate lines in the history.
 
 # Exports
 export EDITOR=vim
 
 export PS1="[\[\e[0;31m\]\u\[\e[m\]@\[\e[0;35m\]\h \[\e[1;32m\]\w\[\e[m\]]> "
-
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/dev
-source /usr/local/bin/virtualenvwrapper.sh
-
-# User variables
-
-if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
-fi
-
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
--- setup.sh
+++ setup.sh
@@ -26,14 +26,8 @@ done
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install applications
-brew cask install gimp
 brew cask install google-chrome
-brew cask install google-drive
-brew cask install intellij-idea
 brew cask install iterm2
-brew cask install java
-brew cask install mactex
-brew cask install slack
 brew cask install spectacle
 brew cask install spotify
 brew cask install visual-studio-code
@@ -41,21 +35,11 @@ brew cask install visual-studio-code
 # Install utilities
 brew install awscli
 brew install bash-completion
-brew install coursier/formulas/coursier
 brew install git
-brew install htop
 brew install jq
-brew install nginx
-brew install node
-brew install python
 brew install sbt
 brew install scala
-brew install tree
 brew install yarn
-
-pip install --upgrade pip
-pip install virtualenv
-pip install virtualenvwrapper
 
 ################
 #Generic Config
@@ -63,7 +47,6 @@ pip install virtualenvwrapper
 
 # Only require a password for git once an hour
 git config --global credential.helper cache
-git config --global credential.helper 'cache --timeout=3600'
 
 #################
 #OS X Preferences
@@ -85,52 +68,16 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Don't create .DS_Store files on network drives
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-#Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -float 2
-
-#Set a shorter Delay until key repeat
-defaults write NSGlobalDomain InitialKeyRepeat -int 12
-
-#Set the function keys to be always on
-defaults write NSGlobalDomain com.apple.keyboard.fnState -int 1;
-
-# Disable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-#Add a context menu item for showing the Web Inspector in web views
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Disable auto-correct
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-
 #Show the ~/Library folder
 chflags nohidden ~/Library
 
 # See ALL the files (and dirs)
 defaults write com.apple.finder AppleShowAllFiles YES
 
-# get rid of cursor blow-up
-defaults write ~/Library/Preferences/.GlobalPreferences CGDisableCursorLocationMagnification -bool YES
-
-# scroll direction
-defaults write -g com.apple.swipescrolldirection -bool false
-
 # Show date
 defaults write com.apple.menuextra.clock DateFormat -string "EEE HH:mm"
 defaults write com.apple.menuextra.clock FlashDateSeparators -int 0
 defaults write com.apple.menuextra.clock IsAnalog -int 0
 
-# remove these useless folders
-sudo find / -name ".DS_Store" -depth -exec rm {} \;
-
 killall SystemUIServer
 echo "Done. Note that some of these changes require a logout/restart to take effect."
-
-#
-# Manual
-#
-
-echo "Set user avatar"
-read
-echo "Set keyboard shortcuts (screenshots, spotlight etc)"
-read
